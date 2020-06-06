7cf373fb0ac7e3fce34d4fabb166fde7e5e5e545
Royce Williams <royce@techsolvency.com>
2020-06-05 23:28:20 -0800

    Use mdxfind if available; more leet

--- gen-descrypt-allsalts.sh
+++ gen-descrypt-allsalts.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-
+#-----------------------------------------------------------------------------
 # Generate all valid descrypts for a given string.
 # From https://github.com/roycewilliams/kens-salty-rainbow
 # License: MIT
+#-----------------------------------------------------------------------------
+MDXFIND=$(which mdxfind 2>/dev/null)
 
 if [ -z "$1" ]; then
     echo "- Usage: $0 [plaintext]"
@@ -12,11 +14,21 @@ else
     >&2 echo "- Processing plaintext: \"${PLAINTEXT}\""
 fi
 
-SALTCHARS=". / 0 1 2 3 4 5 6 7 8 9 A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z"
+#-----------------------------------------------------------------------------
+# If we have mdxfind, use it
+if [ ! -z "${MDXFIND}" -a -x "${MDXFIND}" ]; then
 
-for salt1 in ${SALTCHARS}; do
-    for salt2 in ${SALTCHARS}; do
-        echo -n ${PLAINTEXT} | openssl passwd -stdin -salt ${salt1}${salt2}
+    echo -n ${PLAINTEXT} | mdxfind -h '^DESCRYPT$' -z -f /dev/null stdin 2>/dev/null | egrep '^DESCRYPT' | cut -d\  -f2 | cut -d: -f1
+
+else
+    SALTCHARS=". / 0 1 2 3 4 5 6 7 8 9 A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z"
+
+    # If we don't have mdxfind, use openssl
+    for salt1 in ${SALTCHARS}; do
+        for salt2 in ${SALTCHARS}; do
+            echo -n ${PLAINTEXT} | openssl passwd -stdin -salt ${salt1}${salt2}
+        done
     done
-done
+fi
 
+#-----------------------------------------------------------------------------
---
+++ gen-leet-permutations.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# From https://github.com/roycewilliams/kens-salty-rainbow
+# License: MIT
+
+mdxfind -h '^NULL$' -z -f /dev/null stdin \
+    -R leet-single.rule -R leet-single.rule -R leet-single.rule -R leet-single.rule \
+    -R leet-single.rule -R leet-single.rule -R leet-single.rule -R leet-single.rule \
+    | cut -d: -f2- \
+    | awk '{c[$0]++} END {for (line in c) print line}'
+
