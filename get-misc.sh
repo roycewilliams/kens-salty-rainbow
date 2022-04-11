a6dd5b0101e6de5d90b807e752a33f20c8fb4dc9
Royce Williams <royce@techsolvency.com>
2022-04-11 14:16:20 -0800

    update misc

--- get-misc.sh
+++ get-misc.sh
@@ -9,15 +9,16 @@ echo "- Searching for hashes with miscellaneous properties ..."
 :wq
 # all lower
 wc -l ./misc/descrypt-all-lower.txt
-grep -ev '[A-Z0-9\./ \#]' -r ./lists/ | sed 's/.*descrypt\-//g;s/\-allsalts.txt:/ /g' | awk '{print $2":"$1}' | sort > ./misc/descrypt-all-lower.txt
+grep -v -e '[A-Z0-9\./ \#]' -r ./lists/ | sed 's/.*descrypt\-//g;s/\-allsalts.txt:/ /g' | awk '{print $2":"$1}' | sort > ./misc/descrypt-all-lower.txt
 wc -l ./misc/descrypt-all-lower.txt
 
 # all upper
 wc -l ./misc/descrypt-all-upper.txt
-grep -ev '[a-z0-9\./ \#]' -r ./lists/ | sed 's/.*descrypt\-//g;s/\-allsalts.txt:/ /g' | awk '{print $2":"$1}' | sort > ./misc/descrypt-all-upper.txt
+grep -v -e '[a-z0-9\./ \#]' -r ./lists/ | sed 's/.*descrypt\-//g;s/\-allsalts.txt:/ /g' | awk '{print $2":"$1}' | sort > ./misc/descrypt-all-upper.txt
 wc -l ./misc/descrypt-all-upper.txt
 
 # all digits - rare
 wc -l ./misc/descrypt-all-digits.txt
-grep -ev '[A-Za-z\./ \#]' -r ./lists/ | sed 's/.*descrypt\-//g;s/\-allsalts.txt:/ /g' | awk '{print $2":"$1}' | sort > ./misc/descrypt-all-digits.txt
+grep -v -e '[A-Za-z\./ \#]' -r ./lists/ | sed 's/.*descrypt\-//g;s/\-allsalts.txt:/ /g' | awk '{print $2":"$1}' | sort > ./misc/descrypt-all-digits.txt
 wc -l ./misc/descrypt-all-digits.txt
+
